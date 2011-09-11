fc932edb485846e920360fbf815dcc308049e8ba
mathieucarbou <mathieu.carbou@gmail.com>
2011-09-11 15:07:20 -0400

    using new ovea pipe library

--- src/test/groovy/seleniumHub.groovy
+++ src/test/groovy/seleniumHub.groovy
@@ -1,118 +1,47 @@
 #!/usr/bin/env groovy
 
-import java.util.concurrent.ConcurrentHashMap
+import com.ovea.network.tunnel.Tunnel
 import java.util.concurrent.LinkedBlockingQueue
 import org.testatoo.selenium.server.SeleniumServer
 import org.testatoo.selenium.server.SeleniumServerFactory
+import com.ovea.network.tunnel.TunnelListener
+import com.ovea.network.tunnel.BrokenTunnelException
+import java.util.concurrent.CopyOnWriteArrayList
+import com.ovea.network.util.NetUtils
 
 @GrabResolver(name = 'testatoo', root = 'http://oss.sonatype.org/content/repositories/snapshots/')
 @Grab('org.testatoo:testatoo-selenium:1.0-rc2')
+@Grab('com.ovea:ovea-pipe:1.0.ga-SNAPSHOT')
 
 def LISTEN_PORT = args.length > 0 ? args[0] as int : 4444
 def REMOTE_CONTROL_COUNT = args.length > 1 ? args[1] as int : 5
 def ports = new LinkedBlockingQueue()
 def servers = new TreeMap<Integer, SeleniumServer>()
-def cleaners = new ConcurrentHashMap<Thread, Object>()
+def tunnels = new CopyOnWriteArrayList<Tunnel>()
+def shutdown = false
 
-def showServers = {list ->
-    println "[${Thread.currentThread().name}] Available Selenium Servers: ${(list ? list : ports)}"
+def showServers = {
+    println "[${Thread.currentThread().name}] Available Selenium Servers: ${new ArrayList(ports)}"
 }
 
-def findFreePort = {
-    r = new Random()
-    for (;;) {
-        p = 1025 + r.nextInt(64000)
-        try {
-            new ServerSocket(p).close()
-            return p
-        } catch (IOException ignored) {
-        }
-    }
-}
-
-def clean = {int p, Socket client, Socket rc ->
-    synchronized (ports) {
-        if (!ports.contains(p)) {
-            println "[${Thread.currentThread().name}] Disconnecting client ${client.remoteSocketAddress} from Selenium Server ${p}..."
-            try {
-                client?.close()
-            } catch (e) {}
-            try {
-                rc?.close()
-            } catch (e) {}
-            try {
-                servers[p].stop()
-            } catch (e) {}
-            servers[p] = SeleniumServerFactory.configure().setPort(p).setSingleWindow(true).create()
-            servers[p].start()
-            if (!Thread.currentThread().isInterrupted()) {
-                showServers(new LinkedList(ports) + p)
-            }
-            ports.offer(p)
-        }
-    }
-}
-
-def tunnel = {Socket client, int p ->
-    println "[${Thread.currentThread().name}] Connecting client ${client.remoteSocketAddress} to Selenium Server ${p}..."
-    Socket rc = null
-    try {
-        rc = new Socket('localhost', p as int)
-    } catch (e) {
-        clean(p, client, rc)
-    }
-    Thread reader = null, writer = null, cleaner = null, hook = null;
-    reader = Thread.start "${client.remoteSocketAddress} READER", {
-        while (!Thread.currentThread().isInterrupted()) {
-            try {
-                rc.outputStream.write(client.inputStream.read())
-            } catch (e) {
-                Thread.currentThread().interrupt()
-                writer?.interrupt()
-            }
-        }
-    }
-    writer = Thread.start "${client.remoteSocketAddress}  WRITER", {
-        while (!Thread.currentThread().isInterrupted()) {
-            try {
-                client.outputStream.write(rc.inputStream.read())
-            } catch (e) {
-                Thread.currentThread().interrupt()
-                reader?.interrupt()
-            }
-        }
-    }
-    cleaner = Thread.start client.remoteSocketAddress.toString(), {
-        try {
-            reader.join()
-        } catch (e) {
-            writer.interrupt()
-        }
-        try {
-            writer.join()
-        } catch (e) {
-            reader.interrupt()
-        };
-        clean(p, client, rc)
-    }
-    cleaners.put(cleaner, Void.TYPE)
+def addRC = {
+    int port = NetUtils.findAvailablePort()
+    servers[port] = SeleniumServerFactory.configure().setPort(port).setSingleWindow(true).create()
+    servers[port].start()
+    ports.offer(port)
 }
 
 Runtime.runtime.addShutdownHook(new Thread('CLEANER') {
     @Override
     void run() {
-        cleaners.each {k, v -> k.interrupt()}
+        shutdown = true
+        tunnels.each {Tunnel t -> t.interrupt()}
     }
 })
 
 println "Starting ${REMOTE_CONTROL_COUNT} Selenium Servers..."
 
-REMOTE_CONTROL_COUNT.times {
-    int port = findFreePort()
-    servers[port] = SeleniumServerFactory.configure().setPort(port).setSingleWindow(true).create()
-    servers[port].start()
-    ports.offer(port)
-}
+REMOTE_CONTROL_COUNT.times {addRC()}
 
 showServers()
 
@@ -120,12 +49,46 @@ println "Listening for incoming requests on port ${LISTEN_PORT}..."
 
 def serverSocket = new ServerSocket(LISTEN_PORT)
 for (;;) {
-    socket = null
     try {
-        socket = serverSocket.accept()
+        int port = ports.take() as int
+        println "[${Thread.currentThread().name}] Took port ${port} and wait for client..."
+        showServers()
+        Socket client = serverSocket.accept()
+        Socket rc = new Socket('localhost', port)
+        tunnels << Tunnel.connect(client, rc, new TunnelListener() {
+            void onConnect(Tunnel tunnel) {
+                println "[${Thread.currentThread().name}] Connected client ${client.remoteSocketAddress} to Selenium Server ${port}..."
+            }
+
+            void onClose(Tunnel tunnel) {
+                tunnels.remove(tunnel)
+                println "[${Thread.currentThread().name}] Disconnecting client ${client.remoteSocketAddress} from Selenium Server ${port}..."
+                try {
+                    client.close()
+                } catch (e) {}
+                try {
+                    rc.close()
+                } catch (e) {}
+                try {
+                    servers[port].stop()
+                } catch (e) {}
+                if (!shutdown) {
+                    addRC()
+                    showServers()
+                }
+            }
+
+            void onBroken(Tunnel tunnel, BrokenTunnelException e) {
+                onClose(tunnel)
+            }
+
+            void onInterrupt(Tunnel tunnel) {
+                onClose(tunnel)
+            }
+        })
     } catch (IOException) {
         serverSocket.close()
         break
     }
-    tunnel(socket, ports.take() as int)
 }
+
